21598bb6b746482c9e3912ef589818946a7843e5
sumit <roundup414@gmail.com>
2024-11-21 15:06:14 +0530

    created jsoneditor

---
+++ src/components/JsonEditor.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface JsonEditorProps {
+  jsonInput: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const JsonEditor: React.FC<JsonEditorProps> = ({ jsonInput, handleInputChange }) => {
+  return (
+    <div className="flex-1 p-4">
+      
+      <textarea
+        value={jsonInput}
+        onChange={handleInputChange}
+        rows={20}
+        className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 font-mono"
+      />
+    </div>
+  );
+};
+
+export default JsonEditor;
+
