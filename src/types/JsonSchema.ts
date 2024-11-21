efcc53ca024d641ec432743fbe8b6cc583f0ad4b
sumit <roundup414@gmail.com>
2024-11-21 14:38:36 +0530

    created JsonSchema succesfully

--- src/types/JsonSchema.ts
+++ src/types/JsonSchema.ts
@@ -1,19 +1,16 @@
-export interface FormField {
-    id: string;
-    type: string;
-    label: string;
-    required: boolean;
-    placeholder?: string;
-    options?: { value: string; label: string }[];
-    validation?: {
-      pattern: string;
-      message: string;
-    };
-  }
-  
-  export interface FormSchema {
-    formTitle: string;
-    formDescription: string;
-    fields: FormField[];
-  }
-  
+
+export interface Field {
+  id: string;
+  type: string;
+  label: string;
+  required: boolean;
+  placeholder?: string;
+  options?: { value: string; label: string }[];
+  validation?: { pattern: string; message: string };
+}
+
+export interface FormSchema {
+  formTitle: string;
+  formDescription: string;
+  fields: Field[];
+}
